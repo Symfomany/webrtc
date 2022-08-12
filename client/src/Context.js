30763c134c9442f9222a8070ab4e91262e42b987
Julien Taiwa <taiwa.julien@gmail.com>
2022-08-12 11:19:46 +0200

    pk

--- client/src/Context.js
+++ client/src/Context.js
@@ -5,7 +5,7 @@ import Peer from 'simple-peer';
 const SocketContext = createContext();
 
 // const socket = io('http://localhost:5000');
-const socket = io('https://ia.taiwa.tech');
+const socket = io('https://ia.taiwa.tech:5000');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
